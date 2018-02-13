bcdd1f4534dc2e27244039eba6c2a4230b1fa68b
KAILI <>
2018-02-13 15:17:50 +0800

    initial commit

---
+++ app/src/main/java/com/is416/assignment1/todolist/AddTaskActivity.java
@@ -0,0 +1,13 @@
+package com.is416.assignment1.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task_activity);
+    }
+}
---
+++ app/src/main/java/com/is416/assignment1/todolist/MainActivity.java
@@ -0,0 +1,19 @@
+package com.is416.assignment1.todolist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addNewTask(View view) {
+        Intent newT = new Intent(this, AddTaskActivity.class);
+    }
+}
